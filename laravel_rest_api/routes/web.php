e056d3332bd177a00c9f3936ab6be164c659f5a8
LBenzahia <lakhdar.benzahia@gmail.com>
2017-10-20 20:18:58 +0100

    add simple api

--- laravel_rest_api/routes/web.php
+++ laravel_rest_api/routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::group(['prefix' => 'private/api/v1'], function()
+{
+    Route::get('items', function()
+    {
+        // Matches The "/private/api/v1/" URL
+        return response()->json([
+          ['item_id' => 1,
+          'item_content' => 'item content'],
+          ['item_id' => 2,
+          'item_content' => 'item content'],
+          ['item_id' => 3,
+          'item_content' => 'item content'],
+        ]);
+    });
 });
